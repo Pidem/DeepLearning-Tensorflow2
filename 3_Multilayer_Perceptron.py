aa831d4126ea397cbba8f64744284005b9c8b949
Pierre de Malliard <demalliardpierre@gmail.com>
2019-06-18 20:13:03 -0700

    Multilayer Deep Perceptron with Dropout

--- 3_Multilayer_Perceptron.py
+++ 3_Multilayer_Perceptron.py
@@ -102,12 +102,14 @@ def train_step(images, labels):
 
 model = Model()
 
-        loss = loss_object(labels, predictions)
-    gradients = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
-    train_loss(loss)
-    train_accuracy(labels, predictions)
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
 
 @tf.function
 def test_step(images, labels):
